406cacc73b5b96f402c331b0d0a3035e94461dc8
William Briggs <williambriggs@Williams-MacBook-Air.local>
2022-11-23 15:00:07 +0000

    Add say_hi function; updated index

--- nbdev_tutorial/_modidx.py
+++ nbdev_tutorial/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://WillBriggs-SI.github.io',
                 'git_url': 'https://github.com/WillBriggs-SI/nbdev-tutorial',
                 'lib_path': 'nbdev_tutorial'},
-  'syms': {'nbdev_tutorial.core': {'nbdev_tutorial.core.foo': ('core.html#foo', 'nbdev_tutorial/core.py')}}}
+  'syms': { 'nbdev_tutorial.core': {'nbdev_tutorial.core.foo': ('core.html#foo', 'nbdev_tutorial/core.py')},
+            'nbdev_tutorial.testing': { 'nbdev_tutorial.testing.foo': ('core.html#foo', 'nbdev_tutorial/testing.py'),
+                                        'nbdev_tutorial.testing.say_hi': ('core.html#say_hi', 'nbdev_tutorial/testing.py')}}}
